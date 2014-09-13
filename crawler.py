cfb2b61b10d14b4cec9297bec557e9455d31e93e
Jack You <gmyou71@gmail.com>
2014-09-13 23:07:08 +0900

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -1,19 +1,11 @@
 import urllib2
 from bs4 import BeautifulSoup
-import sys
 
 url = "https://play.google.com/store/apps/collection/topselling_free"
 #url = "https://play.google.com/store/apps/collection/topselling_paid"
 
 f = urllib2.urlopen(url)
 # f = open('sample.html')
-
-def ConvertKorean(s):
-    reload(sys)
-    sys.setdefaultencoding('utf-8')
-    print str(unicode(s))
-
-
 
 page = f.read().decode('utf-8', 'ignore')
 f.close()
