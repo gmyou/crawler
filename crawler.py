af4c222eac1819f3ce814075bfda6b5a0b188835
Jack You <gmyou71@gmail.com>
2014-09-13 23:06:30 +0900

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -1,18 +1,28 @@
-# -*- coding: 949 -*-
-
+import urllib2
 from bs4 import BeautifulSoup
 import sys
-import re
 
-soup = BeautifulSoup(open("sample.html"))
+url = "https://play.google.com/store/apps/collection/topselling_free"
+#url = "https://play.google.com/store/apps/collection/topselling_paid"
+
+f = urllib2.urlopen(url)
+# f = open('sample.html')
+
+def ConvertKorean(s):
+    reload(sys)
+    sys.setdefaultencoding('utf-8')
+    print str(unicode(s))
 
 
-#Sheet Subject
-for string in soup.h1.strings:
-    print(repr(string))
 
+page = f.read().decode('utf-8', 'ignore')
+f.close()
 
-#App List
-for link in soup.find_all('div'):
-    if(link.get('class')=="card-content"):
-        print(link.get('data-docid'))
+soup = BeautifulSoup(page)
+
+# n = soup.findAll('li', attrs={'class': 'new'})
+
+u = soup.findAll('div', attrs={'class': 'card'})
+# print u
+for item in u:
+	print item['data-docid']
