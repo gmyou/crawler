cff38fd3041317cc936a38109411b688da9937c0
Jack You <gmyou71@gmail.com>
2015-02-06 17:17:36 +0900

    add title, hit, date

--- test/soup_test.py
+++ test/soup_test.py
@@ -14,3 +14,9 @@ for link in soup.find_all('tr', attrs={"class": re.compile("^ctl_list")}):
     print link.contents[1].get_text()
     #subject
     print link.find_all('td', attrs={"class":"ctl_sj"})[0].a.get_text()
+    #content
+    print link.find_all('td', attrs={"class":"ctl_sj"})[0].a['title']
+    #hit
+    print link.find_all('td', attrs={"class":"ctl_hit"})[0].get_text()
+    #date
+    print link.find_all('td', attrs={"class":"ctl_date"})[0].get_text()
