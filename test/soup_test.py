6b4a20d85855ad2b9b24b0ee5a37df477e1183fb
Jack You <gmyou71@gmail.com>
2015-02-07 13:26:50 +0900

    add try except

--- test/soup_test.py
+++ test/soup_test.py
@@ -2,21 +2,45 @@
 import re
 from bs4 import BeautifulSoup
 import sys
+from symbol import try_stmt
+from _ctypes import Array
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
 soup = BeautifulSoup(open('../resource/KMug.html'))
 # print (soup.prettify())
 
+def isArray(obj):
+    if (isinstance(obj, Array)):
+        return True
+    else:
+        return False
+
 for link in soup.find_all('tr', attrs={"class": re.compile("^ctl_list")}):
     #id
     #print link.contents[1]
     print link.contents[1].get_text()
+
     #subject
-    print link.find_all('td', attrs={"class":"ctl_sj"})[0].a.get_text()
+    try:
+        print link.find_all('td', attrs={"class":"ctl_sj"})[0].a.get_text()
+    except:
+        print 'N/A'
+        
     #content
-    print link.find_all('td', attrs={"class":"ctl_sj"})[0].a['title']
+    try:
+        print link.find_all('td', attrs={"class":"ctl_sj"})[0].a['title']
+    except:
+        print 'N/A'
+        
     #hit
-    print link.find_all('td', attrs={"class":"ctl_hit"})[0].get_text()
+    try:
+        print link.find_all('td', attrs={"class":"ctl_hit"})[0].a.get_text()
+    except:
+        print 'N/A'
+         
     #date
-    print link.find_all('td', attrs={"class":"ctl_date"})[0].get_text()
+    try:
+        print link.find_all('td', attrs={"class":"ctl_date"})[0].a.get_text()
+    except:
+        print 'N/A'
