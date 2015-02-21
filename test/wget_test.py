13d9950dc2d9b597ebcdbd5377556c4001760121
Jack You <gmyou71@gmail.com>
2015-02-21 12:41:44 -0800

    add main function & arguments

--- test/wget_test.py
+++ test/wget_test.py
@@ -1,16 +1,60 @@
 # -*- coding:utf-8 -*-
 import urllib
 import urllib2
+import sys
+import getopt
 
-url = 'http://kmug.co.kr/board/zboard.php'
+def getHtml(url, values, output):
+	#url = 'http://kmug.co.kr/board/zboard.php'
 
-values = {'id' : 'sale' }
+	#values = {'id' : 'sale' }
 
-data = urllib.urlencode(values)
-req = urllib2.Request(url, data)
-response = urllib2.urlopen(req) 
-the_page = response.read()
+	data = urllib.urlencode(values)
+	req = urllib2.Request(url, data)
+	response = urllib2.urlopen(req) 
+	the_page = response.read()
 
-f = open('../resource/KMug.html', 'w')
+	# f = open('../resource/KMug.html', 'w')
+	f = open(output, 'w')
 
-f.write(the_page)
+	f.write(the_page)
+
+
+# url = 'http://kmug.co.kr/board/zboard.php'
+# values = {'id' : 'sale' }
+# output = '../resource/KMug.html'
+
+# getHtml(url,  values, output)
+
+def main(argv):
+	inputfile = ''
+	outputfile = ''
+	params = ''
+	try:
+		opts, args = getopt.getopt(argv,"hi:o:p:",["ifile=","ofile=","params="])
+	except getopt.GetoptError:
+		print 'test.py -i <url> -o <outputfile> -p <params>'
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt == '-h':
+			print 'test.py -i <url> -o <outputfile> -p <params>'
+			sys.exit()
+		elif opt in ("-i", "--ifile"):
+			inputfile = arg
+		elif opt in ("-o", "--ofile"):
+			outputfile = arg
+		elif opt in ("-p", "--params"):
+			params = arg
+	print 'Get Url is ', inputfile
+	print 'Output file is ', outputfile
+	print 'Parameters are ', params
+
+	#TODO split('&') for Mulit Parameters
+	p = params.split('=')
+	values = {}
+	values[p[0]] = p[1]
+	print values
+	getHtml(inputfile,  values, outputfile)
+
+if __name__ == "__main__":
+	 main(sys.argv[1:])	
