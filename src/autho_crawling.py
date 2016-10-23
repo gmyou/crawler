33e7172a9dab7c8f1c1574af0157eb0f735e0ee9
Jack D. You <gmyou71@gmail.com>
2016-10-23 15:32:22 +0900

    modify autho

--- src/autho_crawling.py
+++ src/autho_crawling.py
@@ -1,26 +1,24 @@
 # -*- coding: utf-8 -*-
 import urllib
 import urllib2
-import cookielib
+import cookielib, requests
 import sys
-from config import *
+from config import get_account
+
+login_url = 'http://tpholic.com/xe/?act=procMemberLogin'
+this_url = 'http://tpholic.com/xe/?mid=ibmsellbuy2&category=1521642'
+
+# login_url = 'http://admin.tripzine.net/admin/login/in'
+# this_url = 'http://admin.tripzine.net/admin/board'
+
+user_id, user_pw = get_account()
+print user_id, user_pw
 
 cj = cookielib.CookieJar()
 opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
 
-login_url = 'http://tpholic.com/xe/main'
-this_url = 'http://tpholic.com/xe/?mid=ibmsellbuy2&category=1521642'
+login_data = urllib.urlencode({'user_id' : user_id, 'password' : user_pw})
+opener.open(login_url, login_data)
 
-values = {'user_id' : user_id,
-      'password' : user_pw }
-
-data = urllib.urlencode(values)
-o = opener.open(login_url, data)
-for cookie in cj:
-   print cookie.name, cookie.value, cookie.domain
-
-f = opener.open(this_url)
-
-the_page = f.read()
-
-print the_page
+resp = opener.open(this_url)
+print resp.read()
